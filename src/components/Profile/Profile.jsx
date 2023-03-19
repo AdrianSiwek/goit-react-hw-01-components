5279946a0c346858fb07adfc687bc1a64f27afb2
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-03-19 01:15:59 +0100

    update Profile.jsx and Profile.module.css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -17,23 +17,24 @@ const Profile = ({username,
                 <p className={styles.name}>{username}</p>
                 <p className={styles.tag}>@{tag}</p>
                 <p className={styles.location}>{location}</p>
-            </div>
+            
 
-  <ul className="stats">
-    <li>
-      <span className="label">Followers</span>
-      <span className="quantity">{followers}</span>
-    </li>
-    <li>
-      <span className="label">Views</span>
-      <span className="quantity">{views}</span>
-    </li>
-    <li>
-      <span className="label">Likes</span>
-      <span className="quantity">{likes}</span>
-    </li>
-  </ul>
-</div>
+                <ul className={styles.stats}>
+                  <li className={styles.items}>
+                    <span className={styles.label}>Followers</span>
+                    <span className={styles.quantity}>{followers}</span>
+                  </li>
+                  <li className={styles.items}>
+                    <span className={styles.label}>Views</span>
+                    <span className={styles.quantity}>{views}</span>
+                  </li>
+                  <li className={styles.items}>
+                    <span className={styles.label}>Likes</span>
+                    <span className={styles.quantity}>{likes}</span>
+                  </li>
+                </ul>
+            </div>
+          </div>
       );
 }
  
